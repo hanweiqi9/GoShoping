cc1c1cc5034f005b1b25b4162b1a5ac4dc5d85de
WangXueJuan <1216747227@qq.com>
2016-03-16 19:27:54 +0800

    xiugai

---
+++ Shoping/classes/Nearby/Controllers/ShopDetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  ShopDetailViewController.h
+//  Shoping
+//
+//  Created by scjy on 16/3/16.
+//  Copyright © 2016年 韩苇棋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShopDetailViewController : UIViewController
+@property (nonatomic, strong) NSString *detailId;
+
+
+@end
